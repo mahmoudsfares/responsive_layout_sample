24a8f9ba64627425f9c88a7c2cc3a5a1f84de699
msaeed <msaeed@minapharm.com>
2024-07-10 15:28:20 +0300

    Remove ResponsiveRowColumn widgets

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:responsive_layout_sample/login/fragments/background_separator.dart';
 import 'package:responsive_layout_sample/login/fragments/my_app_bar.dart';
 import 'package:responsive_layout_sample/login/fragments/submit_button.dart';
-import 'package:responsive_framework/responsive_framework.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -19,52 +18,39 @@ class LoginScreen extends StatelessWidget {
                 child: SingleChildScrollView(
                   child: Column(
                     children: [
-                      ResponsiveRowColumn(
-                        layout: ResponsiveWrapper.of(context).equals(MOBILE)
-                            ? ResponsiveRowColumnType.COLUMN
-                            : ResponsiveRowColumnType.ROW,
-                        children : [
-                          ResponsiveRowColumnItem(
-                            rowFlex: 1,
-                            child: Padding(
-                              padding: const EdgeInsets.fromLTRB(32, 0, 32, 8),
-                              child: TextFormField(
-                                cursorColor: Colors.deepPurpleAccent,
-                                decoration: const InputDecoration(
-                                  focusedBorder: UnderlineInputBorder(
-                                    borderSide:
-                                    BorderSide(color: Colors.deepPurpleAccent),
-                                  ),
-                                  labelText: 'Username',
-                                  labelStyle: TextStyle(color: Colors.grey),
-                                ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(32, 0, 32, 8),
+                        child: TextFormField(
+                          cursorColor: Colors.deepPurpleAccent,
+                          decoration: const InputDecoration(
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide:
+                              BorderSide(color: Colors.deepPurpleAccent),
+                            ),
+                            labelText: 'Username',
+                            labelStyle: TextStyle(color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+                          child: TextFormField(
+                            cursorColor: Colors.deepPurpleAccent,
+                            decoration: const InputDecoration(
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                BorderSide(color: Colors.deepPurpleAccent),
+                              ),
+                              labelText: 'Password',
+                              labelStyle: TextStyle(color: Colors.grey),
+                              suffixIcon: IconButton(
+                                icon: Icon(Icons.visibility),
+                                color: Colors.grey,
+                                onPressed: null,
                               ),
                             ),
                           ),
-                            ResponsiveRowColumnItem(
-                              rowFlex: 1,
-                              child: Padding(
-                                padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
-                                child: TextFormField(
-                                  cursorColor: Colors.deepPurpleAccent,
-                                  decoration: const InputDecoration(
-                                    focusedBorder: UnderlineInputBorder(
-                                      borderSide:
-                                      BorderSide(color: Colors.deepPurpleAccent),
-                                    ),
-                                    labelText: 'Password',
-                                    labelStyle: TextStyle(color: Colors.grey),
-                                    suffixIcon: IconButton(
-                                      icon: Icon(Icons.visibility),
-                                      color: Colors.grey,
-                                      onPressed: null,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                        ]
-                      ),
+                        ),
                       const SubmitButton('LOGIN'),
                     ],
                   ),
